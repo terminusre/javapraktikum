3d2ace561acfe987509024625c0fa418509bebbf
soenke kracht <sk@nrpl.com>
2015-10-12 16:40:38 +0200

    prog

--- A1-WS1515ConvolutionFilter/src/application/Filter.java
+++ A1-WS1515ConvolutionFilter/src/application/Filter.java
@@ -1,8 +1,54 @@
 package application;
 
 import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
 
 public abstract class Filter {
+
+	private Image _image;
+	PixelReader _reader;
+	private WritableImage _filteredImage;
+	PixelWriter _writer;
+
+	public Filter() {
+		try {
+			Image _image = new Image(getClass().getResourceAsStream(
+					"Blumenwiese.jpg"));
+			_reader = _image.getPixelReader();
+			//Color col = _reader.getColor(1, 1);// (readX, readY);
+
+			_filteredImage = new WritableImage((int)_image.getWidth(), (int) _image.getHeight());
+			_writer = _filteredImage.getPixelWriter();
+			//_writer.setColor(1, 1, col);// (posX,poxY,color);
+
+			/*
+			 * System.out.println("data " + (int) _image.getWidth() + " / " +
+			 * (int) _image.getHeight());
+			 */
+
+		} catch (Exception e) {
+			System.out.println("Satz mit X, das war wohl nix!");
+		}
+	}
+
+	abstract double[][] generateFilter();
+
+	private double[][] init() {
+		double[][] filter;
+		int lines = (int) _image.getHeight();
+		int linewidth = (int) _image.getWidth();
+
+		filter = new double[lines][];
+
+		for (int i = 0; i < lines; i++) {
+			filter[i] = new double[linewidth];
+		}
+
+		return filter;
+	}
 
 	public static final Filter IDENTITY = new Filter() {
 		@Override
@@ -12,7 +58,7 @@ public abstract class Filter {
 
 		@Override
 		double[][] generateFilter() {
-			// TODO Auto-generated method stub
+			//double[][] filter = init();
 			return null;
 		}
 
@@ -26,13 +72,11 @@ public abstract class Filter {
 
 		@Override
 		double[][] generateFilter() {
-			// TODO Auto-generated method stub		
+			// TODO Auto-generated method stub
 			return null;
 		}
 
 	};
-
-	abstract double[][] generateFilter();
 
 	public static final Filter STRONG_BLUR = new Filter() {
 		public String toString() {
@@ -140,9 +184,9 @@ public abstract class Filter {
 		return null;
 	}
 
-	public Filter() {
-		// TODO
-	}
+	/*
+	 * public Filter() { // TODO }
+	 */
 
 	public Filter(double bias) {
 		// TODO
