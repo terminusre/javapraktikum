0c4587e4e716f97993fc59bd8303e6e0bb4bf6ae
soenke kracht <sk@nrpl.com>
2015-11-04 09:25:22 +0100

    lauffaehig

--- A2-WS1516ReadingAndParsingIataCodes/src/iata/airline/IataAirlineCollectionReader.java
+++ A2-WS1516ReadingAndParsingIataCodes/src/iata/airline/IataAirlineCollectionReader.java
@@ -1,25 +1,46 @@
 package iata.airline;
 
-import java.net.URL;
-import java.util.Collection;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import iata.AbstractIataCollectionReader;
 import iata.Iata;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class IataAirlineCollectionReader extends AbstractIataCollectionReader {
 
 	public Collection<? extends Iata> readLocalCollection() {
+
+		Scanner scanner = null;
+
+		try {
+			URL url = getClass().getResource("iata_airline.htm");
+			scanner = new Scanner(url.openStream(), "UTF-8");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println(scanner.next());
+
 		Pattern p = Pattern.compile("a*b");
 		Matcher m = p.matcher("aaaaab");
 		boolean b = m.matches();
+
+		LinkedList airlines = new LinkedList();
+
 		IataAirline a = null;
-		return null;
+
+		scanner.close();
+		return airlines;
 	}
 
 	protected Collection<? extends Iata> readSingleCollection(URL url) {
-		return null;
+		return readLocalCollection();
 	}
 
 }
--- A2-WS1516ReadingAndParsingIataCodes/src/iata/airport/IataAirportCollectionReader.java
+++ A2-WS1516ReadingAndParsingIataCodes/src/iata/airport/IataAirportCollectionReader.java
@@ -3,17 +3,40 @@ package iata.airport;
 import iata.AbstractIataCollectionReader;
 import iata.Iata;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class IataAirportCollectionReader extends AbstractIataCollectionReader {
 
 	public Collection<? extends Iata> readLocalCollection() {
+		Scanner scanner = null;
+
+		try {
+			URL url = getClass().getResource("A.htm");
+			scanner = new Scanner(url.openStream(), "UTF-8");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Pattern p = Pattern.compile("a*b");
+		Matcher m = p.matcher("aaaaab");
+		boolean b = m.matches();
+
+		LinkedList airports = new LinkedList();
+
 		IataAirport a = null;
-		return null;
+
+		scanner.close();
+		return airports;
 	}
 
 	protected Collection<? extends Iata> readSingleCollection(URL url) {
-		return null;
+		return readLocalCollection();
 	}
 }
