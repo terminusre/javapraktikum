bfd387884b78e6bba57dadca62f5d63870f1999e
soenke kracht <sk@nrpl.com>
2016-01-08 16:31:51 +0300

    prog

--- A4-WS1516GUI-JavaFX-Reisebuchung/src/gui/ReiseBuchungController.java
+++ A4-WS1516GUI-JavaFX-Reisebuchung/src/gui/ReiseBuchungController.java
@@ -1,12 +1,36 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class ReiseBuchungController extends Application {
+
+	// left panel
+	@FXML
+	TextField kundenauswahlfeld = new TextField();
+	@FXML
+	Button flugEntfernen  = new Button();
+
+	@FXML
+	Label abflughafen = new Label();
+	@FXML
+	Label ankunftflughafen = new Label();
+	@FXML
+	Label abflugszeit = new Label();
+	@FXML
+	Label ankunftszeit = new Label();
+	@FXML
+	Label flugdauer = new Label();
+	@FXML
+	Label gesamtpreis = new Label();
+	// right panel
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -18,6 +42,9 @@ public class ReiseBuchungController extends Application {
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			primaryStage.setTitle("Reisen");
+			
+			Button schwerVonKape;
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- A4-WS1516GUI-JavaFX-Reisebuchung/src/gui/ReiseBuchungSample.java
+++ A4-WS1516GUI-JavaFX-Reisebuchung/src/gui/ReiseBuchungSample.java
@@ -1,29 +1,29 @@
-package gui;
-	
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
-
-
-
-public class ReiseBuchungSample extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			HBox root = (HBox)FXMLLoader.load(getClass().getResource("ReiseBuchung.fxml"));
-			Scene scene = new Scene(root); //,400,400);
-			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-			primaryStage.setTitle("Reisen");
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+//package gui;
 //	
-//	public static void main(String[] args) {
-//		launch(args);
+//import javafx.application.Application;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Scene;
+//import javafx.scene.layout.HBox;
+//import javafx.stage.Stage;
+//
+//
+//
+//public class ReiseBuchungSample extends Application {
+//	@Override
+//	public void start(Stage primaryStage) {
+//		try {
+//			HBox root = (HBox)FXMLLoader.load(getClass().getResource("ReiseBuchung.fxml"));
+//			Scene scene = new Scene(root); //,400,400);
+//			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			primaryStage.setScene(scene);
+//			primaryStage.show();
+//			primaryStage.setTitle("Reisen");
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//		}
 //	}
-}
+////	
+////	public static void main(String[] args) {
+////		launch(args);
+////	}
+//}
